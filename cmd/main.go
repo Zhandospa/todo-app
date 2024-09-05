504e6909956a09b68ccbb93de3f098127c931a92
Zhandospa <ptizza.ferma@gmail.com>
2024-09-05 18:39:22 +0500

    1 серия

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	"github.com/Zhandospa/todo-app"
+	"github.com/Zhandospa/todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatal("error occoured wjile running http server: %s", err.Error())
 	}
 }
